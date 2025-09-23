232e683e8753358b5116b3b4e094144c62383457
mherreray <mherreray@alumnos.unex.es>
2025-09-23 21:51:24 +0200

    Agregar archivos de configuración y estructura inicial del proyecto MDAI

---
+++ MDAI-Final/src/test/java/com/example/MDAI_Proyecto/MdaiProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MDAI_Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdaiProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
