e0d32e754b8e4a2bbd626b285a885cb7b789bf18
mherreray <mherreray@alumnos.unex.es>
2025-10-14 12:17:06 +0200

    Add MySQL configuration and user repository tests; implement user model methods

--- MDAI-Final/src/main/java/com/example/MDAI_Proyecto/data/repository/UserRepository.java
+++ MDAI-Final/src/main/java/com/example/MDAI_Proyecto/data/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.example.MDAI_Proyecto.data.repository;
 import com.example.MDAI_Proyecto.data.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<Usuario, Long> {
+    List<Usuario> findUserByUsername(String username);
 }
