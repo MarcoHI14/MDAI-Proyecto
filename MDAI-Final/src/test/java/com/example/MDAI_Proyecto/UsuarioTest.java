7137b34b07eb27b717f8e54916a6fa937ac4ed71
mherreray <mherreray@alumnos.unex.es>
2025-10-14 18:02:13 +0200

    Add new HTML pages for user login, registration, and admin dashboard; update workspace configuration

--- MDAI-Final/src/test/java/com/example/MDAI_Proyecto/UsuarioTest.java
+++ MDAI-Final/src/test/java/com/example/MDAI_Proyecto/UsuarioTest.java
@@ -15,7 +15,7 @@ class UsuarioTest {
     private UserRepository userRepository;
 
     @Test
-    void crearUsuarios() {
+    void crearUsuario() {
         Usuario usuario = new Usuario();
         usuario.setUsername("Testeador1");
         usuario.setPassword("1234");
@@ -31,9 +31,52 @@ class UsuarioTest {
         userRepository.delete(guardado);
         boolean existe = userRepository.findById(guardado.getId()).isPresent();
         assertThat(existe).isFalse();
+    }
+
+    @Test
+    void crearUsuarios() {
 
         Usuario usuario2 = new Usuario();
         Usuario usuario3 = new Usuario();
-    }
+        Usuario usuario4 = new Usuario();
 
+        usuario2.setUsername("Testeador2");
+        usuario2.setPassword("1234");
+        usuario2.setEmail("testeador2@gmail.com");
+
+        usuario3.setUsername("Testeador3");
+        usuario3.setPassword("2345");
+        usuario3.setEmail("testeador3@gmail.com");
+
+        usuario4.setUsername("Testeador4");
+        usuario4.setPassword("3456");
+        usuario4.setEmail("testeador4@gmail.com");
+
+        userRepository.save(usuario2);
+        userRepository.save(usuario3);
+        userRepository.save(usuario4);
+
+        assertThat(userRepository.findAll()).hasSize(3);
+
+        String nombre2=userRepository.findById(usuario2.getId()).get().getUsername();
+        assertThat(nombre2).isEqualTo("Testeador2");
+        String password2=userRepository.findById(usuario2.getId()).get().getPassword();
+        assertThat(password2).isEqualTo("1234");
+        String email2=userRepository.findById(usuario2.getId()).get().getEmail();
+        assertThat(email2).isEqualTo("testeador2@gmail.com");
+
+        String nombre3=userRepository.findById(usuario3.getId()).get().getUsername();
+        assertThat(nombre3).isEqualTo("Testeador3");
+        String password3=userRepository.findById(usuario3.getId()).get().getPassword();
+        assertThat(password3).isEqualTo("2345");
+        String email3=userRepository.findById(usuario3.getId()).get().getEmail();
+        assertThat(email3).isEqualTo("testeador3@gmail.com");
+
+        String nombre4=userRepository.findById(usuario4.getId()).get().getUsername();
+        assertThat(nombre4).isEqualTo("Testeador4");
+        String password4=userRepository.findById(usuario4.getId()).get().getPassword();
+        assertThat(password4).isEqualTo("3456");
+        String email4=userRepository.findById(usuario4.getId()).get().getEmail();
+        assertThat(email4).isEqualTo("testeador4@gmail.com");
+    }
 }
