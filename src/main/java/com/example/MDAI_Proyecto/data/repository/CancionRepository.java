65680dc54484bec0a9af662caeda40ad4b353000
JuanJoseGalindoCotano <jgalindom@alumnos.unex.es>
2025-10-16 18:53:53 +0200

    Updated Canciontest

--- src/main/java/com/example/MDAI_Proyecto/data/repository/CancionRepository.java
+++ src/main/java/com/example/MDAI_Proyecto/data/repository/CancionRepository.java
@@ -21,7 +21,9 @@ import com.example.MDAI_Proyecto.data.model.Cancion;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CancionRepository extends CrudRepository<Cancion, Long> {
-
+    Optional<Cancion> findByTitulo(String titulo);
 }
