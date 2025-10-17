63967bb3d0c5890b2435cc15637dbfc91fa466d3
mherreray <mherreray@alumnos.unex.es>
2025-10-17 13:32:10 +0200

    Enhance ArtistaTest with additional assertions and duplicate artist handling

--- src/test/java/com/example/MDAI_Proyecto/ArtistaTest.java
+++ src/test/java/com/example/MDAI_Proyecto/ArtistaTest.java
@@ -9,8 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -59,5 +58,21 @@ public class ArtistaTest {
         assertEquals("Soy un artista español de pop rock.", artistaGuardado2.getBiografia());
         assertEquals(usuarioGuardado2.getId(), artistaGuardado2.getIdArtista());
         assertEquals("Leiva", artistaGuardado2.getUsuario().getUsername());
+
+        assertNotEquals(artistaGuardado.getIdArtista(), artistaGuardado2.getIdArtista());
+
+        try {        // Intentar añadir dos veces al mismo artista
+            Artista artistaDuplicado = new Artista();
+            artistaDuplicado.setUsuario(usuarioGuardado);
+            artistaDuplicado.setBiografia("Intento duplicado de Bunbury.");
+            artistaRepository.save(artistaDuplicado);
+            fail("Se pudo añadir dos veces al mismo artista, ERROR, no debería ser posible.");
+        } catch (Exception e) {
+            System.out.println("Excepción capturada correctamente al intentar duplicar un artista: " + e.getMessage());
+        }
+        artistaRepository.deleteAll();
+        userRepository.deleteAll();
+        assertEquals(0, artistaRepository.count());
+        assertEquals(0, userRepository.count());
     }
 }
