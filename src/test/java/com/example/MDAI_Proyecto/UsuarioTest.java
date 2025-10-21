164f229577a208db577e3864ce8cf78b50b91086
JuanJoseGalindoCotano <jgalindom@alumnos.unex.es>
2025-10-21 12:02:17 +0200

    Added @AutoConfigureDatabases to UsuarioTest and CancionTest to avoid defaulting to H2 database. Eliminated H2 dependencies from pom.xml

--- src/test/java/com/example/MDAI_Proyecto/UsuarioTest.java
+++ src/test/java/com/example/MDAI_Proyecto/UsuarioTest.java
@@ -4,6 +4,7 @@ import com.example.MDAI_Proyecto.data.model.Usuario;
 import com.example.MDAI_Proyecto.data.repository.UsuarioRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UsuarioTest {
 
     @Autowired
