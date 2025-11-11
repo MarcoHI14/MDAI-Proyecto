8f5a753f7c7bd8cdd5b00604b55a55c579f556b3
mherreray <mherreray@alumnos.unex.es>
2025-11-11 18:54:20 +0100

    Add documentation for CancionService interface to describe its purpose and methods

--- src/main/java/com/example/MDAI_Proyecto/data/services/CancionService.java
+++ src/main/java/com/example/MDAI_Proyecto/data/services/CancionService.java
@@ -5,6 +5,10 @@ import com.example.MDAI_Proyecto.data.model.Cancion;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Servicio para la entidad Cancion.
+ * Proporciona métodos para realizar operaciones relacionadas con Cancion.
+ */
 public interface CancionService {
 
     public Optional<Cancion> findById(Long id);
