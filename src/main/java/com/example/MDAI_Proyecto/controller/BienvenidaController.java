a9d4ec6ed4bbf036fcb8e96ff6f5562b14b20437
JuanJoseGalindoCotano <jgalindom@alumnos.unex.es>
2025-11-25 18:48:38 +0100

    Updated controllers and turnt logo into a home button

---
+++ src/main/java/com/example/MDAI_Proyecto/controller/BienvenidaController.java
@@ -0,0 +1,36 @@
+package com.example.MDAI_Proyecto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BienvenidaController {
+
+    public BienvenidaController() {
+        System.out.println("\t BienvenidaController inicializado");
+    }
+
+    @GetMapping("/Buscar")
+    public String mostrarBuscar() {
+        System.out.println("\t Recojo la petición de /Buscar");
+        return "BuscarCanciones";
+    }
+
+    @GetMapping("/Playlists")
+    public String mostrarPlaylists() {
+        System.out.println("\t Recojo la petición de /Playlists");
+        return "GestorPlaylists";
+    }
+
+    @GetMapping("/Artistas")
+    public String mostrarArtistas() {
+        System.out.println("\t Recojo la petición de /Artistas");
+        return "BuscarArtistas";
+    }
+
+    @GetMapping("/Micuenta")
+    public String mostrarMiCuenta() {
+        System.out.println("\t Recojo la petición de /Micuenta");
+        return "MiCuenta";
+    }
+}
---
+++ src/main/java/com/example/MDAI_Proyecto/controller/CancionController.java
@@ -0,0 +1,14 @@
+package com.example.MDAI_Proyecto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CancionController {
+
+    public CancionController() {
+        System.out.println("\t CancionController inicializado");
+    }
+
+}
